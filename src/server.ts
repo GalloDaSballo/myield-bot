009fa4754fd15e2ddebf9568eb3efd7024d53cc6
Entreprenerd <valeranijr@gmail.com>
2021-06-01 00:34:30 +0200

    fix: swap to higher rate

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,14 @@
 import cron from "node-cron";
 import { BigNumber, Contract, providers, Wallet } from "ethers";
 import dotenv from "dotenv";
-
-import vaults, { VAULT_ABI, STRAT_ABI } from "./vaults";
 import dcaVaults, { DCA_VAULT_ABI, DCA_STRAT_ABI } from "./dcaVaults";
 
-import { MyieldVault } from "./MyieldVault";
 import { AAVEUSDCRewards } from "./AAVEUSDCRewards";
-import { DCAVault, Vault } from "./types";
+import { DCAVault } from "./types";
 import { MyieldDCAVault } from "./MyieldDCAVault";
 
 const MIN_MATIC_USD = "1515151520000000000"; // Higher = Matic goes down
-const MIN_MATIC_BTC = "38530670500000000000000";
+const MIN_MATIC_BTC = "4057270000000000000000000";
 
 const MIN_TO_SWAP = "1000"; // 0.1 USDC
 const MIN_TO_REDISTRIBUTE = "1000"; // 100 sats, about 30 cents
@@ -27,7 +24,6 @@ const WALLET_PK = process.env.PK;
 const { BLOCKVIGIL_KEY } = process.env;
 
 // 0.10 // With 3500$ this should happens over 20 times a day
-const MIN_REWARDS = BigNumber.from("100000000000000000");
 
 // 0.001 which should happen 10 times per hjour
 const MIN_DCA_REWARDS = BigNumber.from("1000000000000000");
@@ -36,6 +32,7 @@ const wallet = new Wallet(
   WALLET_PK,
   new providers.JsonRpcProvider(BLOCKVIGIL_KEY)
 );
+console.log("wallet", wallet.address);
 
 const checkDCAVault = async (vault: DCAVault) => {
   const vaultContract = new Contract(
@@ -57,6 +54,9 @@ const checkDCAVault = async (vault: DCAVault) => {
     const toSwap = await vaultContract.toSwap();
     console.log("toSwap", toSwap.toString());
     if (toSwap.gt(MIN_TO_SWAP)) {
+      const price = MIN_PRICE[vault.need.symbol];
+      console.log("swap price", price);
+
       const swapTx = await (
         await vaultContract.swapToNeed(MIN_PRICE[vault.need.symbol])
       ).wait();
@@ -98,10 +98,12 @@ const checkDCAVault = async (vault: DCAVault) => {
     if (currentRewards.gt(MIN_DCA_REWARDS)) {
       console.log("Will harvest");
       // Get price quote
+      const price = MIN_PRICE[vault.want.symbol];
+      console.log("price", price);
 
       // Ask for harvest
       const harvestTx = await (
-        await stratContract.harvest(MIN_PRICE[vault.want.symbol], {
+        await stratContract.harvest(price, {
           gasLimit: 5000000,
         })
       ).wait();
@@ -143,8 +145,8 @@ const checkDCAVault = async (vault: DCAVault) => {
 };
 
 /** Once every 2 mins */
-cron.schedule("*/5 * * * *", async () => {
-  console.log("Every 5 minutes, for dcaVaults");
+cron.schedule("*/1 * * * *", async () => {
+  console.log("Every 1 minutes, for dcaVaults");
 
   const dcaVaultsToLoop = [...dcaVaults];
   while (dcaVaultsToLoop.length) {
